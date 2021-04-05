50d78fd464f3c9dee55fbb335c4136536b1e6e66
shubham88fru <shubham88fru@yahoo.com>
2021-04-05 10:51:33 +0530

    video thumnails and videolist

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
@@ -22,7 +23,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        {this.state.videos.length}
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
